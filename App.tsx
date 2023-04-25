49862d641103532549230423a92fe92d57d65080
Hussain Nagaria <hussainbhaitech@gmail.com>
2023-04-26 00:33:09 +0530

    chore: play with emotion/react

--- App.tsx
+++ App.tsx
@@ -1,20 +1,41 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StatusBar } from "expo-status-bar";
+import { StyleSheet, Text, View } from "react-native";
+import styled, { css } from "@emotion/native";
+import { ThemeProvider } from "@emotion/react";
+
+const theme = {
+  "gray.900": "#171717",
+};
+
+const Badge = styled.View`
+  background-color: ${(props) => props.theme["gray.900"]};
+  padding: 1px 6px;
+  border-radius: 100px;
+`;
+
+const BadgeText = styled.Text`
+  color: white;
+  font-size: 12px;
+`;
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+    <ThemeProvider theme={theme}>
+      <View style={styles.container}>
+        <Badge>
+          <BadgeText>Gamma</BadgeText>
+        </Badge>
+      </View>
       <StatusBar style="auto" />
-    </View>
+    </ThemeProvider>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
   },
 });
